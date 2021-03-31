9cb224847ed8aa79b2f9904f0e385f813e39736a
Hugo Mozo <hmozo@outlook.com>
2021-03-31 15:16:33 +0200

    Ejercicio 9

---
+++ moda/src/moda/e9/Employee.java
@@ -0,0 +1,21 @@
+package moda.e9;
+
+public class Employee {
+	private String name;
+	private Integer age;
+	
+	public String getName() {
+		return name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public Employee(String name, Integer age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	
+}
